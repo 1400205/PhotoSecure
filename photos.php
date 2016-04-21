c28ba5742809be5cfb61cbb6c7aa18b69fed8ad8
1400205 <1400205@rgu.ac.uk>
2016-04-21 06:32:01 +0100

    change connection

--- photos.php
+++ photos.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+$login_user= $_SESSION["username"];
 ?>
 <?php
 	include("check.php");
 	include("userphotos.php");
-$login_user= $_SESSION["username"];
+
 ?>
 
 <!doctype html>
