290e249c530721775a12f3d9eaa2650d5b183b1a
1400205 <1400205@rgu.ac.uk>
2016-04-23 23:42:51 +0100

    insert procedure

--- submit.php
+++ submit.php
@@ -1,5 +1,10 @@
 <?php
 $msg = "";
+//connections
+include("connection.php"); //Establishing connection with our database
+$mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if(!$mysqli) die('Could not connect$: ' . mysqli_error());
+
 if(isset($_POST["submit"]))
 {
     $name = $_POST["username"];
@@ -17,9 +22,13 @@ if(isset($_POST["submit"]))
     }
     else
     {
+        //call procedure
+        $result = $mysqli->query("CALL sp_insertUserDetails($email,$username,$password)");
+        $result = $mysqli->query( 'SELECT @userID' );
+        //if(!$result) die("CALL failed: (" . $mysqli->errno . ") " . $mysqli->error);
         //echo $name." ".$email." ".$password;
-        $query = mysqli_query($db, "INSERT INTO usersSecure (username, email, password) VALUES ('$name', '$email', '$password')")or die(mysqli_error($db));
-        if($query)
+       // $query = mysqli_query($db, "INSERT INTO usersSecure (username, email, password) VALUES ('$name', '$email', '$password')")or die(mysqli_error($db));
+        if($result)
         {
             $msg = "Thank You! you are now registered. click <a href='index.php'>here</a> to login";
         }
