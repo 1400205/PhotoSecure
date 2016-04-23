878f23d3c6bfbb4eec564fd37115a6d68d096ffc
1400205 <1400205@rgu.ac.uk>
2016-04-23 18:12:44 +0100

    change connection

--- connection.php
+++ connection.php
@@ -3,5 +3,5 @@ define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'ba3dcb56418003');
 define('DB_PASSWORD', '5055d853');
 define('DB_DATABASE', 'BJTS');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$db = new mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
