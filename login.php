c019ef1dc4cd09f654873ce9d2b9c6f305032117
1400205 <1400205@rgu.ac.uk>
2016-04-24 12:27:58 +0100

    insert procedure

--- login.php
+++ login.php
@@ -2,64 +2,43 @@
 <?php
 session_start();
 ?>
-
 <?php
-$error = ""; //Variable for storing our errors.
-if(isset($_POST["submit"])){
-	if (empty($_POST["username"]) || empty($_POST["password"])) {
-		$error = "Both fields are required.";
-		echo "Both fields are required.";
-	}else{
-
-		include("connection.php"); //Establishing connection with our database
-		$mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-		//if(!$mysqli) die('Could not connect$: ' . mysqli_error());
-
-		//test connection
-		if ($mysqli->connect_errno) {
-			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-		}
-
-		//create procedure
-
-		if (!$mysqli->query("DROP PROCEDURE IF EXISTS getUserID") ||
-			!$mysqli->query('CREATE PROCEDURE getUserID(IN loc_username varchar(255),
-			 IN loc_password varchar(255), OUT loc_userID int)
-			BEGIN
- 			SELECT `userID` INTO loc_userID FROM userssecure WHERE username = loc_username
-     	AND password = loc_password;END;')) {
-			echo "Stored procedure creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-
-		// Define $username and $password
-		$username=$_POST['username'];
-		$password=$_POST['password'];
-
-		// Prepare OUT parameters
-		$mysqli->query("SET @userID=0");
-
-		if ( !$mysqli->query("CALL getUserID('$username','$password',@userID)"))  {
-			echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-
-		if ($res = $mysqli->query("SELECT @userID as p_out")) {
-			$row = $res->fetch_assoc();}
-
-		if(mysqli_num_rows($res) == 1 ){
-			$_SESSION['userid']=$row['p_out'];
-
-			$_SESSION['username'] = $username; // Initializing Session
-
-			header("location: photo.php"); // Redirecting
+	include("connection.php"); //Establishing connection with our database
+	
+	$error = ""; //Variable for storing our errors.
+	if(isset($_POST["submit"]))
+	{
+		if(empty($_POST["username"]) || empty($_POST["password"]))
+		{
+			$error = "Both fields are required.";
+		}else
+		{
+			// Define $username and $password
+			$username=$_POST['username'];
+			$password=$_POST['password'];
 
 
-				//echo $_SESSION['userid'];
+			
+			//Check username and password from database
+			$sql="SELECT userID FROM userssecure WHERE username='$username' and password='$password'";
+			$result=mysqli_query($db,$sql);
+			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+			$userid=$row['userID'];//Get user ID
+			
+			//If username and password exist in our database then create a session.
+			//Otherwise echo error.
+			
+			if(mysqli_num_rows($result) == 1)
+			{
+				$_SESSION['username'] = $username; // Initializing Session
+				$_SESSION["userid"] = $userid;//user id assigned to session global variable
+				header("location: photos.php"); // Redirecting To Other Page
+			}else
+			{
+				$error = "Incorrect username or password.";
 			}
 
-
-
-
 		}
-
 	}
+
 ?>
