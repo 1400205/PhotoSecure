8cea25a38956200ba744dac74214292916d6efea
1400205 <1400205@rgu.ac.uk>
2016-04-24 07:44:48 +0100

    insert procedure

--- login.php
+++ login.php
@@ -2,14 +2,16 @@
 <?php
 session_start();
 ?>
+
 <?php
+$error = ""; //Variable for storing our errors.
+
+if (empty($_POST["username"]) || empty($_POST["password"])) {
+$error = "Both fields are required.";
+}
+
 	include("connection.php"); //Establishing connection with our database
 $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if(!$mysqli) die('Could not connect$: ' . mysqli_error());
 
-$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"])) {
-		if (empty($_POST["username"]) || empty($_POST["password"])) {
-			$error = "Both fields are required.";
-		}
-	}
+
