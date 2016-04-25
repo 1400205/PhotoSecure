f53f2f6ee03d5db51a0f36e7da5c8bd26f7de042
1400205 <1400205@rgu.ac.uk>
2016-04-25 22:51:14 +0100

    session timeout

--- photos.php
+++ photos.php
@@ -11,20 +11,6 @@ $login_user= $_SESSION["username"];
 ?>
 
 
-<?php
-//session time sub
-if($_SESSION ['timeout']+ 60 < time()){
-
-	//session timed out
-	session.session_destroy();
-	Header("Location:logout.php");
-}else{
-	//reset session time
-	$_SESSION['timeout']=time();
-}
-?>
-
-
 
 <!doctype html>
 <html>
