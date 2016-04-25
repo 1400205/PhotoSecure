8080307a65f55de1773d1aa387537467c9542ff3
1400205 <1400205@rgu.ac.uk>
2016-04-25 19:56:36 +0100

    insert procedure

--- login.php
+++ login.php
@@ -93,15 +93,7 @@ session_start();
 			{
 				$_SESSION['username'] = $username; // Initializing Session
 				$_SESSION["userid"] = $userid;//user id assigned to session global variable
-				//session IP binding
-				//$IP=$_SERVER['REMOTE_ADDR'];
-
-				// Function to get the client IP address
-
-
-				//echo $_SERVER['HTTP_USER_AGENT'];
-				//echo $_SESSION['username'];
-				//header("location: photos.php"); // Redirecting To Other Page
+				header("location: photos.php"); // Redirecting To Other Page
 			}
 
 
