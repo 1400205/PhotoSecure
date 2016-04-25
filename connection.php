b0b8b1a00e4c8c7577f2c5273cb37d6ef3fffe36
1400205 <1400205@rgu.ac.uk>
2016-04-25 14:03:22 +0100

    insert procedure

--- connection.php
+++ connection.php
@@ -4,4 +4,10 @@ define('DB_USERNAME', 'ba3dcb56418003');
 define('DB_PASSWORD', '5055d853');
 define('DB_DATABASE', 'BJTS');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+//function xssafe
+function xssafe($data,$encoding='UTF-8'){
+    return htmlspecialchars($data,ENT_QUOTES|ENT_HTML401|ENT_HTML401);
+}
+
 ?>
