05ccaa0ed300999ced29496b6caa680c0abfd005
1400205 <1400205@rgu.ac.uk>
2016-04-25 14:06:19 +0100

    insert procedure

--- connection.php
+++ connection.php
@@ -10,4 +10,8 @@ function xssafe($data,$encoding='UTF-8'){
     return htmlspecialchars($data,ENT_QUOTES|ENT_HTML401|ENT_HTML401);
 }
 
+// function to clean output
+function xecho($data){
+    echo xssafe($data);
+};
 ?>
