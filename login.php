2f308311124f051be1967421d2bcf9f2631c2358
1400205 <1400205@rgu.ac.uk>
2016-04-25 16:35:44 +0100

    insert procedure

--- login.php
+++ login.php
@@ -95,28 +95,11 @@ session_start();
 				//session IP binding
 				//$IP=$_SERVER['REMOTE_ADDR'];
 				// Function to get the client IP address
-				function get_client_ip() {
-					$ipaddress = '';
-					if (getenv('HTTP_CLIENT_IP'))
-						$ipaddress = getenv('HTTP_CLIENT_IP');
-					else if(getenv('HTTP_X_FORWARDED_FOR'))
-						$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-					else if(getenv('HTTP_X_FORWARDED'))
-						$ipaddress = getenv('HTTP_X_FORWARDED');
-					else if(getenv('HTTP_FORWARDED_FOR'))
-						$ipaddress = getenv('HTTP_FORWARDED_FOR');
-					else if(getenv('HTTP_FORWARDED'))
-						$ipaddress = getenv('HTTP_FORWARDED');
-					else if(getenv('REMOTE_ADDR'))
-						$ipaddress = getenv('REMOTE_ADDR');
-					else
-						$ipaddress = 'UNKNOWN';
-					return $ipaddress;
-				}
 
-				echo $IP=getenv('HTTP_CLIENT_IP');
+
+				//echo $IP=getenv('HTTP_CLIENT_IP');
 				//echo $_SESSION['username'];
-				//header("location: photos.php"); // Redirecting To Other Page
+				header("location: photos.php"); // Redirecting To Other Page
 			}
 
 
