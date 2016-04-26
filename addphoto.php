722eb99e21808386fe379aaa942105bd2347643c
1400205 <1400205@rgu.ac.uk>
2016-04-26 10:13:06 +0100

    session timeout

--- addphoto.php
+++ addphoto.php
@@ -97,7 +97,7 @@ if(isset($_POST["submit"]))
                 $msg = "Your image was not uploaded";
             }
         }else{
-            $msg = "Your image was not uploaded. We can only accept JPEG or PNG images.";
+            $msg = "Your image was not uploaded. We can only accept JPEG or PNG images."." ".$uploaded_ext;
         }
 
         //echo $name." ".$email." ".$password;
