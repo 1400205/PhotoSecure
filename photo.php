f8fd1099db1ce5fa83539d52961a1caab7d5f016
1400205 <1400205@rgu.ac.uk>
2016-04-28 07:07:26 +0100

    session timeout

--- photo.php
+++ photo.php
@@ -48,7 +48,7 @@ include("connection.php");
         }
         else{
             //bind parameter
-            $stm->bindparam('i', $_GET['id']);
+            $stm->bind_param('i',$_GET['id']);
             $stm->execute();
             $result=$stm->get_result();
             $row=$result->fetch_assoc();
